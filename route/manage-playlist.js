f3e8c0542696077c0f4156fd4f89659e54652d1b
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-06-05 13:35:09 -0700

    made get playlist route but not dynamic

--- route/manage-playlist.js
+++ route/manage-playlist.js
@@ -1,9 +1,29 @@
 'use strict';
 
 const router = require('express').Router();
-
-// create playlist
+var request = require('request');
 
 router.post('/create', (req, res) => {
-  
-})
+
+});
+
+router.get('/getPlaylist', (req, res) => {
+
+  request({
+    url: 'https://api.spotify.com/v1/users/1216797299/playlists/12D1JAQakvKog8BoCpDFw6?market=ES',
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + 'BQACZ0zvx3HWkL9ykvbxhoe9zKf8BzZkAsj0o6Qklr1JxqP-3XmAcnH_iw0IIiRhQrV0W5Y1my28ZRBn8OrmRtaZ7mvfrpL7Y64qQ9uTZbuWrnLyRq0iteVr9SbwYkBJd0mGRFdHpSTaeg'
+    }
+  }, function(error, response, body){
+    if(error) {
+      console.log(error);
+    } else {
+      console.log(response.statusCode, body);
+      res.send(body);
+    }
+  });
+
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 
 const app = require('express')();
 const authRouter = require('./route/user-auth');
+const manageRouter = require('./route/manage-playlist')
 
 app.use('/', authRouter);
+
+app.use('/', manageRouter);
 
 app.listen(8888, () => {
   console.log('Up on 8888');
